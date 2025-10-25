411731042c5d70faed49c1d10c3224cbfd8e0e92
James Espinosa <kingkongjay1@gmail.com>
2025-10-25 14:53:00 +0100

    added test and webbapp.py

--- webapp.py
+++ webapp.py
@@ -13,10 +13,14 @@ st.title("SASS SAAS Platform")
 st.subheader("Your Software Solution")
 st.write("Welcome to our SASS SAAS application!")
 
-# input testing
-st.write("[Learn more >](https://roblox.com)")
+# # input testing
+# st.write("[Learn more >](https://roblox.com)")
 
 
 if st.button("Click Me"):
-    st.write("Button was clicked!")
-    # Add your button action here
+    st.write("[Button was clicked!](https://roblox.com)")
+
+
+# my button runs williams scripts - which sends a request which goes back to me.
+if st.button("Run Williams Script"):
+    st.switch_page("test/ollama_chat_test_forjames")
