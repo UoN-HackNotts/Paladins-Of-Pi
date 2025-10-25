49baaafeba59ca66e51fb962b5554b36ee7f5ba0
EE <williamencabopimentel@gmail.com>
2025-10-25 23:52:08 +0100

    Small fixes

--- ollama_chat.py
+++ ollama_chat.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 ## GET REQUEST (FROM WEBSITE)
 # user input source
-FRONTEND_URL = "http://localhost:8000/generate" # might be 8501
+FRONTEND_URL = "http://localhost:8501/generate" # might be 8501
 LLM_URL = "http://localhost:11434/api/generate" # ollama
+LLM_TIMEOUT = 40
 
 @app.route("/generate", methods=["GET"])
 def generate(): # handler for HTTP GET/generate
@@ -23,31 +24,10 @@ def generate(): # handler for HTTP GET/generate
     story(user_text, True)
 
     """
-    ## LLM ONLY
-    # exception handling (WIP)
-    params = {"q": "python", # q=query, carries the search term/main text the client is asking the server about
-            "page": 2} # pagination: tells server which page of results are desired
-
-    try:
-        resp = requests.get(LLM_URL, params=params)
-
-        # check for errors, specifically the HTTP status code
-        # if between 200-299, method does nothing
-        # if 4xx (client) or 5xx (server) then an HTTPError is raised
-        # why?: prevents the program from silently continuing when something goes wrong
-        resp.raise_for_status()
-
-        user_text = resp.text
-
-    except requests.exceptions.HTTPError as err:
-        pass
-    """
-
-    """
     LLM output
     """
     payload = {"model": "phi3", "prompt": user_text, "stream": False} # sends the text as a prompt (TEMPORARY)
-    llm_resp = requests.post(LLM_URL, json=payload, timeout=15)
+    llm_resp = requests.post(LLM_URL, json=payload, timeout=LLM_TIMEOUT)
     llm_resp.raise_for_status()
     llm_json = llm_resp.json() # gets raw json file of llm's output
 
@@ -58,7 +38,7 @@ def generate(): # handler for HTTP GET/generate
         "ai_text": ai_text,
         
 
-    }), 200
+    }), 200 # flag for "okay"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
