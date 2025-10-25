9710b0f141e738ec9b40f525ef94d88579f3a8f2
James Espinosa <kingkongjay1@gmail.com>
2025-10-25 16:01:15 +0100

    3 - ollama

--- webapp.py
+++ webapp.py
@@ -1,17 +1,18 @@
 import streamlit as st
+from ollama_chat import query_ollama
 
 # Set the website name properly
 st.set_page_config(
-    page_title="SASS SAAS Platform",
+    page_title="Sass SaaS Platform",
     page_icon="📊",
     layout="centered",
     initial_sidebar_state="expanded"
 )
 
 # Main content
-st.title("SASS SAAS Platform")
+st.title("Sass SaaS Platform")
 st.subheader("Your Software Solution")
-st.write("Welcome to our SASS SAAS application!")
+st.write("Welcome to our Sass SaaS application!")
 
 # # input testing
 # st.write("[Learn more >](https://roblox.com)")
@@ -24,3 +25,6 @@ if st.button("Click Me"):
 # my button runs williams scripts - which sends a request which goes back to me.
 if st.button("Run Williams Script"):
     st.switch_page("test/ollama_chat_test_forjames")
+
+# text input to run williams script
+st.text_input("Enter your script input:", key="script_input")
